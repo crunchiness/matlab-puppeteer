db5ff02a95f3759c635405222dd4fca025b8959b
Ingvaras Merkys <ingvaras@gmail.com>
2015-06-23 12:26:34 +0100

    updated motor commands

--- client.m
+++ client.m
@@ -1,7 +1,8 @@
 port = 6789;
-%ip = '10.0.1.1';
-ip = '127.0.0.1';
+ip = '10.0.1.1';
+%ip = '127.0.0.1';
 ctrl = ev3control(port, ip);
-ctrl.motor('A', 360);
-ctrl.motor('D', 360);
-ctrl.exit();
+ctrl.motor_init('A');
+ctrl.motor_init('D');
+ctrl.motor_backward('A');
+ctrl.motor_forward('D');
--- ev3control.m
+++ ev3control.m
@@ -16,28 +16,57 @@ classdef ev3control
         end
         
         function beep(self)
-            json_str = savejson('command', struct('cmd', 'beep'), 'Compact', 1);
+            json_str = savejson('command', struct('cmd', 'beep', 'dev', 'brick'), 'Compact', 1);
             fopen(self.client);
             fwrite(self.client, json_str);
             fclose(self.client);
         end
         
         function buzz(self)
-            fopen(self.client);
-            fwrite(self.client, 'No');
-            fclose(self.client);
-        end
-        
-        function motor(self, which, degrees)
-            json_str = savejson('command', struct('cmd', 'motor', 'which', which, 'degrees', degrees), 'Compact', 1);
+            json_str = savejson('command', struct('cmd', 'buzz', 'dev', 'brick'), 'Compact', 1);
             fopen(self.client);
             fwrite(self.client, json_str);
             fclose(self.client);
         end
         
         function exit(self)
+            json_str = savejson('command', struct('cmd', 'exit', 'dev', 'brick'), 'Compact', 1);
             fopen(self.client);
-            fwrite(self.client, 'Exit');
+            fwrite(self.client, json_str);
+            fclose(self.client);
+        end
+        
+        function motor_init(self, where)
+            json_str = savejson('command', struct('cmd', 'init', 'dev', 'motor', 'port', where), 'Compact', 1);
+            fopen(self.client);
+            fwrite(self.client, json_str);
+            fclose(self.client);
+        end
+        
+        function motor_forward(self, which)
+            json_str = savejson('command', struct('cmd', 'forward', 'dev', 'motor', 'port', which), 'Compact', 1);
+            fopen(self.client);
+            fwrite(self.client, json_str);
+            fclose(self.client);
+        end
+        
+        function motor_backward(self, which)
+            json_str = savejson('command', struct('cmd', 'backward', 'dev', 'motor', 'port', which), 'Compact', 1);
+            fopen(self.client);
+            fwrite(self.client, json_str);
+            fclose(self.client);
+        end
+        
+        function motor_stop(self, which)
+            json_str = savejson('command', struct('cmd', 'stop', 'dev', 'motor', 'port', which), 'Compact', 1);
+            fopen(self.client);
+            fwrite(self.client, json_str);
+            fclose(self.client);
+        end
+        
+        function malformed(self)
+            fopen(self.client);
+            fwrite(self.client, 'shit')
             fclose(self.client);
         end
     end
